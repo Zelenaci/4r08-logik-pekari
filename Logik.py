4d287d61dc753da86e5f62d4e7d8455a2f1e98a5
Jakub Vasicek <vas35137@lp613.spseol.cz>
2019-03-05 11:07:26 +0100

    FrameFix

--- Logik.py
+++ Logik.py
@@ -7,31 +7,32 @@ Created on Tue Mar  5 10:02:14 2019
 """
 
 #import tkinter as tk
-from tkinter import Frame, Button, Radiobutton, Tk, W, N, NW, DISABLED, ACTIVE,Label
+from tkinter import Frame, Button, Radiobutton, Tk, W, N, NW, DISABLED, ACTIVE,Label, Canvas
 
 
 
 class App():
     
-    def __init__(self,master):
-        #hlavní okno
-        frame = Frame(master)
-        frame.grid()
-        
-        #vrchní skryté buttony 
-        hiddenframe=Frame(master).grid(column=0,row=0)
+    def __init__(self, master):
         hidden_btns = []
-        for i in range(5):
-            hidden_btns.append(Button(hiddenframe, bg="black", state=DISABLED).grid(column=i,row=0,padx=1,pady=1))
-        
-        activeframe=Frame(master).grid(column=0,row=1)
         play_btns = []
+        
+        # vrchní skryté buttony 
+        hiddenframe = Frame(master, width=320, height=320)
+        hiddenframe.grid(column=0,row=0)
+        for i in range(5):
+            hidden_btns.append(Button(hiddenframe, bg="black", state=DISABLED))
+            hidden_btns[i].grid(column=i,row=0,padx=1,pady=1)
+
+        # Herní buttony
+        activeframe = Frame(master, width=320, height=320)
+        activeframe.grid(column=0,row=1)
         for y in range(10):
             row = []
             for x in range(5):
-                row.append(Button(activeframe, bg="grey").grid(column=x,row=y,padx=1,pady=1))
+                row.append(Button(activeframe, bg="grey"))
+                row[x].grid(column=x,row=y,padx=1,pady=1)
             play_btns.append(row)
-                
         
         
         
